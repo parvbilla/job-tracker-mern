fc704c92691c26f85d9007378cf91748dc2557b7
parvbilla <parvbilla2004@gmail.com>
2026-02-25 12:58:36 +0530

    backend5

--- backend/controllers/jobController.js
+++ backend/controllers/jobController.js
@@ -1,5 +1,16 @@
 const Job = require("../models/Job");
 
+
+exports.createJob = async (req, res) => {
+  try {
+    const job = await Job.create(req.body);
+    res.status(201).json(job);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+
 // Get all jobs
 exports.getJobs = async (req, res) => {
   const jobs = await Job.find().sort({ appliedDate: -1 });
--- backend/routes/jobRoutes.js
+++ backend/routes/jobRoutes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const { createJob } = require("../controllers/jobController");
 const protect = require("../middleware/authMiddleware");
 const {
   getJobs,
